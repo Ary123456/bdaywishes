1d28d5cfd33e13bf3903f2eca464e73e5c824f75
Ary123456 <84840151+Ary123456@users.noreply.github.com>
2021-10-03 10:15:25 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x = document.getElementById("myAudio");
+
+function The_newImg() {
+    fabric.Image.fromURL("Aryan.png", function (Img) {
+        block_image_object = Img;
+
+        block_image_object.scaletoWidth(700);
+        block_image_object.scaletoHeight(510);
+        block_image_object.set({
+        top:0,
+        left:0
+        });
+     canvas.add(block_image_object);
+    });	
+	
+}
+
+function playSound() {
+x.play();
+}
